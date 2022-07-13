a4f83e686301c73418561aba78fa5d1ea400f214
Shantalia <71754706+Stp155906@users.noreply.github.com>
2022-07-13 14:39:31 -0700

    Update app.py

--- app.py
+++ app.py
@@ -30,12 +30,6 @@ mpl.use("agg")
 from matplotlib.backends.backend_agg import RendererAgg
 _lock = RendererAgg.lock
 
-from st_aggrid import AgGrid
-import pandas as pd
-
-df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
-AgGrid(df)
-
 
 
 
